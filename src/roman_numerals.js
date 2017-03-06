cdac4d68e00e7907e403db312c17629e03343920
Erin Bergman <ebergman@pillartechnology.com>
2017-03-06 11:23:08 -0500

    1-90, +tests

--- src/roman_numerals.js
+++ src/roman_numerals.js
@@ -1,5 +1,10 @@
 let translateToRoman = function (numeral) {
     const romans = {
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
         "V": 5,
         "IV": 4,
         "I": 1
--- test/roman_numerals.spec.js
+++ test/roman_numerals.spec.js
@@ -9,8 +9,8 @@ describe("The file roman_numerals.js should exist", () => {
 });
 
 
-describe("roman_numerals translates from arabic numbers to roman; ten and under", () => {
-    it("should return an 'I' when given 1", () =>{
+describe("roman_numerals translates from arabic numbers to roman; under ten", () => {
+    it("should return an 'I' when given 1", () => {
         expect(ro_num_translate(1)).to.equal("I");
     });
 
@@ -24,5 +24,41 @@ describe("roman_numerals translates from arabic numbers to roman; ten and under"
 
     it("should return 'V' when given 5", () => {
         expect(ro_num_translate(5)).to.equal("V");
+    });
+
+    it("should return 'VI' when given 6", () => {
+        expect(ro_num_translate(6)).to.equal("VI");
+    });
+
+    it("should return 'VIII' when given 8", () => {
+        expect(ro_num_translate(8)).to.equal("VIII");
+    });
+
+    it("should return 'IX' when given 9", () => {
+        expect(ro_num_translate(9)).to.equal("IX");
+    });
+});
+
+describe("roman_numerals translates from arabic numbers to roman; ten to 100", () => {
+    it("should return 'X' when given 10", () => {
+        expect(ro_num_translate(10)).to.equal("X");
+    });
+
+    it("should return 'XL' when given 40", () => {
+        expect(ro_num_translate(40)).to.equal("XL");
+    });
+
+    it("should return 'L' when given 50", () => {
+        expect(ro_num_translate(50)).to.equal("L");
+    });
+
+    it("should return 'XC' when given 90", () => {
+        expect(ro_num_translate(90)).to.equal("XC");
+    });
+});
+
+describe("Boundary tests", () => {
+    it("should not return 'x' lowercase when given 10", () => {
+        expect(ro_num_translate(10)).to.not.equal("x");
     });
 });
